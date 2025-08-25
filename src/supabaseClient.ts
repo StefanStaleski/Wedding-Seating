9e0cae2c67f4fc75882fc1027e660e74247bf17f
Stefan Staleski <stefan.staleski@exo.mk>
2025-08-25 16:26:10 +0200

    update supabaseClient

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://meppclqbvtjeslczpvdk.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lcHBjbHFidnRqZXNsY3pwdmRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxMzA3NzIsImV4cCI6MjA3MTcwNjc3Mn0.O0kVa9kVqKaITiiMGPBxAqqZEHBgXOpV59lUk-wMTkQ';
+
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
